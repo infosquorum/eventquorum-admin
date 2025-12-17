6408de24b8a5d6e5470c15bc7b8e42528cb6551c
BorisYkb <byakoue@gmail.com>
2025-12-17 00:43:48 +0000

    Fix Data fetche problème

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -9,7 +9,7 @@ export const API_CONFIG = {
    * En développement : http://localhost:5000
    * En production : URL de votre serveur Azure
    */
-  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://app-api-staging-frc-eventquorum.azurewebsites.net',
 
   /** URL de base Azure Blob Storage pour les médias */
   BLOB_BASE_URL: 'https://ststagingfrceventquorum.blob.core.windows.net/medias',
